1355e9150d1816e259b1aa398140bd9ac1657e8d
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2025-01-12 17:51:37 +0100

    generate keypair and public address from public key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod eth_wallet;
+
 fn main() {
-    println!("Hello, world!");
+    let (secret_key, pub_key) = eth_wallet::generate_keypair();
+
+    println!("secret key: {}", hex::encode(&secret_key.as_ref()));
+    println!("pub key: {}", &pub_key.to_string());
+
+    let pub_address = eth_wallet::public_key_address(&pub_key);
+    println!("public address: {:?}", pub_address);
 }
